498f63dd80ef3b7ec4843fb279134307b09c2022
Zevyion <90081905+Centurie@users.noreply.github.com>
2025-09-21 11:47:18 +0545

    Update multiclass_dec_bond.py

--- multiclass_dec_bond.py
+++ multiclass_dec_bond.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,14 +7,13 @@ def plot_multiclass_decision_boundary(model, X, y):
     x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
     y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
-    cmap = plt.cm.Spectral
 
     X_test = torch.from_numpy(np.c_[xx.ravel(), yy.ravel()]).float()
-    y_pred = F.softmax(model(X_test), dim=1)
+    y_pred = F.softmax(model(X_test), dim=1)   # now works
     _, y_pred = y_pred.max(dim=1)
     y_pred = y_pred.reshape(xx.shape)
+
     plt.contourf(xx, yy, y_pred, cmap=plt.cm.Spectral, alpha=0.8)
     plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
-
